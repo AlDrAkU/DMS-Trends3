8148f5201021f2f217a48f53d1e433a992705bee
Razvan Brs <razvanbouros@AirMac.local>
2024-04-28 14:07:08 +0200

    updated

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask, request, jsonify
 from flasgger import Swagger
 import pika
 import json
-from utils import build_response_message
+from utils import build_response_message, map_to_json
 import secrets
-
+import xml.etree.ElementTree as ET
+from data_access.models import docTypeModel, InvoiceModel, PaycheckModel, EarningItem, DeductionItem, InvoiceItem, InvoiceSummary
 app = Flask(__name__)
 swagger = Swagger(app)
 
@@ -45,14 +46,8 @@ def queue():
 
     # Declare a queue
     channel.queue_declare(queue="xml_queue")
-
-    # Publish the XML data to the queue
     channel.basic_publish(exchange="", routing_key="xml_queue", body=xml_data)
-
-    # Declare the status_queue
     channel.queue_declare(queue="status_queue")
-
-    # Prepare the status message
     status_message = build_response_message(
         correlation_id, "XML queued successfully", "no error message"
     )
@@ -64,7 +59,7 @@ def queue():
 
     connection.close()
 
-    return jsonify({"status": "XML queued successfully"}), 200
+    return jsonify({"status": f"XML {correlation_id} queued successfully"}), 200
 
 
 @app.route("/dequeue", methods=["GET"])
@@ -83,8 +78,6 @@ def dequeue():
         )
     )
     channel = connection.channel()
-
-    # Declare the same queue
     channel.queue_declare(queue="xml_queue")
     # add message to response que
     # Get the next message from the queue
@@ -94,26 +87,137 @@ def dequeue():
         # Acknowledge the message
         channel.basic_ack(method_frame.delivery_tag)
 
-        # Declare the status_queue
         channel.queue_declare(queue="status_queue")
 
-        # Prepare the status message
-        status_message = build_response_message(
-            "correlation_id", "XML dequeued successfully", body.decode()
-        )
+        data = map_to_json(body)
+
+        # Load the dictionary into the Pydantic model
+        try:
+            doc_type = data.get("documentType")
+            if doc_type == "INV-01":
+                invoice_items_data = data.get("items", {})
+                invoice_summary_data = data.get("summary", {})
+
+                # Parse invoice items
+                item_data = invoice_items_data.get('item', {})
+                invoice_item = InvoiceItem(
+                    description=item_data.get('description'),
+                    size=item_data.get('size'),
+                    finish=item_data.get('finish'),
+                    quantity=int(item_data.get('quantity')),
+                    unitPrice=float(item_data.get('unitPrice')),
+                    total=float(item_data.get('total'))
+                )
+
+                # Parse invoice summary
+                invoice_summary = InvoiceSummary(
+                    subTotal=float(invoice_summary_data.get('subTotal')),
+                    discount=float(invoice_summary_data.get('discount')),
+                    totalLessDiscount=float(invoice_summary_data.get('totalLessDiscount')),
+                    taxRate=float(invoice_summary_data.get('taxRate')),
+                    totalTax=float(invoice_summary_data.get('totalTax')),
+                    balanceDue=float(invoice_summary_data.get('balanceDue'))
+                )
+
+                doc_model = InvoiceModel(
+                    documentType=data.get('documentType'),
+                    companyName=data.get('header', {}).get('companyName'),
+                    address=data.get('header', {}).get('address'),
+                    website=data.get('header', {}).get('website'),
+                    phone=data.get('header', {}).get('phone'),
+                    name=data.get('billTo', {}).get('name'),
+                    billToAddress=data.get('billTo', {}).get('address'),
+                    email=data.get('billTo', {}).get('email'),
+                    billToPhone=data.get('billTo', {}).get('phone'),
+                    number=int(data.get('invoiceInfo', {}).get('number')),
+                    date=data.get('invoiceInfo', {}).get('date'),
+                    dueDate=data.get('invoiceInfo', {}).get('dueDate'),
+                    items=[invoice_item],
+                    summary=invoice_summary,
+                    instruction=data.get('termsAndInstructions', {}).get('instruction'),
+                    warranty=data.get('termsAndInstructions', {}).get('warranty')
+                )
+
+
+
+            elif doc_type == "Pay-01":
+                earnings_data = data.get('earnings', {})
+                deductions_data = data.get('deductions', {})
+
+                # Parse earnings
+                earnings = EarningItem(**earnings_data.get('basicSalary', {}))
+
+                # Parse deductions
+                deductions = DeductionItem(**deductions_data.get('tax', {}))
+
+                # fill in  the paycheck pydantic model with the values
+                doc_model = PaycheckModel(
+                    documentType=data.get('documentType'),
+                    title=data.get('header', {}).get('title'),
+                    periodStart=data.get('header', {}).get('periodStart'),
+                    periodEnd=data.get('header', {}).get('periodEnd'),
+                    processingDate=data.get('header', {}).get('processingDate'),
+                    name=data.get('employeeInformation', {}).get('name'),
+                    position=data.get('employeeInformation', {}).get('position'),
+                    department=data.get('employeeInformation', {}).get('department'),
+                    earnings=earnings,
+                    deductions=deductions,
+                    netPay=float(data.get('netPay')),
+                    companyName=data.get('employerInformation', {}).get('companyName'),
+                    address=data.get('employerInformation', {}).get('address')
+                )
+
+            else:
+                doc_model = docTypeModel(**data)
+            
+        except Exception as e:
+            connection.close()
+            return jsonify({"error": "Failed to parse XML: {}".format(str(e))}), 400
+
+        correlation_id = secrets.token_hex(4)
 
         # Publish the status message to the status_queue
+        channel.queue_declare(queue="status_queue")
+        status_message = build_response_message(
+            correlation_id, "XML dequeued successfully", doc_model.dict()
+        )
         channel.basic_publish(
             exchange="", routing_key="status_queue", body=json.dumps(status_message)
         )
 
         connection.close()
-        return jsonify(status_message), 200
+        return jsonify(doc_model.model_dump()), 200
     else:
         connection.close()
         return jsonify({"status": "No more messages in the queue"}), 200
 
 
+
+@app.route("/store", methods=["POST"])
+def store():
+    """
+    Store XML in a database
+    ---
+    responses:
+      200:
+        description: XML stored successfully
+    """
+    # get xml from queue
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            "localhost", 5672, "/", pika.PlainCredentials("admin", "admin")
+        )
+    )
+
+    # validation
+
+    # saving document to storage
+
+    # saving document uuid, name and timestamp and status to db
+
+    # post status update to status queue
+
+    return jsonify({"status": "XML stored successfully"}), 200
+
 if __name__ == "__main__":
-    print(app.url_map)
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import datetime
+import xml.etree.ElementTree as ET
 def build_response_message(correlation_id, status, message):
     response_message = {
         "correlation_id": correlation_id,
@@ -7,3 +8,39 @@ def build_response_message(correlation_id, status, message):
         "message": message
     }
     return response_message
+
+def map_to_json(body):
+    # Parse the XML body into DocTypeModel
+    xml_data = body.decode()  # Assuming body contains XML as string
+    xml_data = xml_data.replace("{", "").replace("}", "")
+    root = ET.fromstring(xml_data)
+    # Convert the ElementTree into a dictionary
+    # Initialize an empty dictionary to hold the parsed data
+    data = {}
+    # Iterate through each child element of the root
+    for child in root:
+        # Check if the child element has sub-elements
+        if len(child) > 0:
+            # If yes, initialize a sub-dictionary to hold its sub-elements
+            sub_data = {}
+            # Iterate through each sub-element
+            for sub_child in child:
+                # Check if the sub-element has sub-elements
+                if len(sub_child) > 0:
+                    # If yes, initialize a sub-sub-dictionary to hold its sub-elements
+                    sub_sub_data = {}
+                    # Iterate through each sub-sub-element
+                    for sub_sub_child in sub_child:
+                        # Add the sub-sub-element's tag and text to the sub-sub-dictionary
+                        sub_sub_data[sub_sub_child.tag] = sub_sub_child.text.strip() if sub_sub_child.text else None
+                    # Add the sub-sub-dictionary to the sub-dictionary under the tag of the sub-element
+                    sub_data[sub_child.tag] = sub_sub_data
+                else:
+                    # If the sub-element has no sub-elements, directly add its tag and text to the sub-dictionary
+                    sub_data[sub_child.tag] = sub_child.text.strip() if sub_child.text else None
+            # Add the sub-dictionary to the main data dictionary under the tag of the child element
+            data[child.tag] = sub_data
+        else:
+            # If the child element has no sub-elements, directly add its tag and text to the main data dictionary
+            data[child.tag] = child.text.strip() if child.text else None
+    return data
